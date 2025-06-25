7e74bd436462132b6e9712d7d14604cd225ed70a
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-06-25 13:59:48 +0900

    feat: HandlerMethodArgumentResolver 구현(step2)

---
+++ src/main/java/roomescape/member/LoginMemberArgumentResolver.java
@@ -0,0 +1,31 @@
+package roomescape.member;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final MemberService memberService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(LoginMember.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest, WebDataBinderFactory binderFactory)
+            throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String token = memberService.extractTokenFromCookie(request.getCookies());
+        return memberService.checkLogin(token);
+    }
+}
