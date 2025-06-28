d4b1451421df2623b12ffc7bcdf773d109f27be4
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-06-28 13:30:50 +0900

    feat: ExceptionHandler 추가

---
+++ src/main/java/roomescape/exception/controller/GlobalExceptionHandler.java
@@ -0,0 +1,56 @@
+package roomescape.exception.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import roomescape.exception.CustomException;
+import roomescape.exception.ErrorCode;
+import roomescape.exception.ErrorResponse;
+import roomescape.exception.ValidErrorResponse;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 1) Validation 예외 처리
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        log.info("ValidationException occurred: {}", e.getMessage());
+        Map<String, String> validation = new HashMap<>();
+        for (FieldError fieldError : e.getFieldErrors()) {
+            validation.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        ValidErrorResponse response = new ValidErrorResponse(HttpStatus.BAD_REQUEST.toString(), "잘못된 요청입니다.",
+                validation);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    /**
+     * 2) Custom 예외 처리
+     */
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        log.info("CustomException occurred: {}", e.getMessage());
+        ErrorCode errorCode = e.getErrorCode();
+        ErrorResponse response = new ErrorResponse(errorCode, errorCode.getMessage());
+        return ResponseEntity.status(errorCode.getStatus()).body(response);
+    }
+
+    /**
+     * 3) 그 외 예외 처리
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("Unhandled Exception occurred: {}", e.getMessage());
+        ErrorResponse response = new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+}
