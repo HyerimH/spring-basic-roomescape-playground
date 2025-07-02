d7758c03954c395cfa59e74619e6a0c3bb3f1d27
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-07-02 13:42:09 +0900

    refactor: TimeDao 삭제 및 Repository 생성

--- src/main/java/roomescape/global/exception/ErrorCode.java
+++ src/main/java/roomescape/global/exception/ErrorCode.java
@@ -31,9 +31,14 @@ public enum ErrorCode {
 
     // 회원
 
-    USER_NOT_FOUND(HttpStatus.UNAUTHORIZED, "해당 사용자를 찾을 수 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 사용자를 찾을 수 없습니다."),
 
-    INVALID_ROLE(HttpStatus.BAD_REQUEST, "역할이 존재하지 않습니다.");
+    INVALID_ROLE(HttpStatus.BAD_REQUEST, "역할이 존재하지 않습니다."),
+
+    INVALID_EMAILPASSWORD(HttpStatus.UNAUTHORIZED, "이메일 또는 비밀번호가 잘못되었습니다."),
+
+    // 예약
+    RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 예약을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
