2568cb982e903cde06a73c9088553b4107689e80
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-07-12 02:48:02 +0900

    refactor: 토큰에 만료 시간 추가

--- src/main/java/roomescape/auth/JwtTokenProvider.java
+++ src/main/java/roomescape/auth/JwtTokenProvider.java
@@ -3,6 +3,7 @@ package roomescape.auth;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import roomescape.domain.member.Member;
@@ -12,12 +13,16 @@ public class JwtTokenProvider {
 
     @Value("${roomescape.auth.jwt.secret}")
     private String secretKey;
+    @Value("${roomescape.auth.jwt.expiration}")
+    private long expirationTime;
 
     public String createToken(Member member) {
         return Jwts.builder()
                 .setSubject(member.getId().toString())
                 .claim("name", member.getName())
                 .claim("role", member.getRole())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
                 .signWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
                 .compact();
     }
