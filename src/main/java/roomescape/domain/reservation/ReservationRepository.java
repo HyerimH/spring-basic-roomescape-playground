fc13a3680252e317bf9cabfe8dffd6859ee05a9e
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-07-12 13:33:50 +0900

    refactor: 조회 메서드에 Fetch Join 추가

--- src/main/java/roomescape/domain/reservation/ReservationRepository.java
+++ src/main/java/roomescape/domain/reservation/ReservationRepository.java
@@ -3,13 +3,22 @@ package roomescape.domain.reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import roomescape.domain.theme.Theme;
 import roomescape.domain.time.Time;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+    @Query("SELECT r FROM Reservation r " +
+           "JOIN FETCH r.theme " +
+           "JOIN FETCH r.time " +
+           "WHERE r.date = :date AND r.theme.id = :themeId")
     List<Reservation> findByDateAndThemeId(String date, Long themeId);
 
+    @Query("SELECT r FROM Reservation r " +
+           "JOIN FETCH r.theme " +
+           "JOIN FETCH r.time " +
+           "WHERE r.member.id = :memberId")
     List<Reservation> findByMemberId(Long memberID);
 
     boolean existsByDateAndThemeAndTime(String date, Theme theme, Time time);
