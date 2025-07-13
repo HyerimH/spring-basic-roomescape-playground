81d675a5b790be5848ba2536e86cfb501c02303f
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-07-13 23:34:44 +0900

    refactor: Reservation에 Lazy, fetch join 적용

--- src/main/java/roomescape/domain/reservation/Reservation.java
+++ src/main/java/roomescape/domain/reservation/Reservation.java
@@ -2,6 +2,7 @@ package roomescape.domain.reservation;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,15 +27,15 @@ public class Reservation {
     @Column(nullable = false)
     private String date;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "time_id", nullable = false)
     private Time time;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "theme_id", nullable = false)
     private Theme theme;
 
--- src/main/java/roomescape/domain/reservation/ReservationRepository.java
+++ src/main/java/roomescape/domain/reservation/ReservationRepository.java
@@ -1,5 +1,7 @@
 package roomescape.domain.reservation;
 
+import io.micrometer.common.lang.NonNullApi;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -7,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import roomescape.domain.theme.Theme;
 import roomescape.domain.time.Time;
 
+@NonNullApi
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     @Query("SELECT r FROM Reservation r " +
@@ -18,8 +21,22 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     @Query("SELECT r FROM Reservation r " +
            "JOIN FETCH r.theme " +
            "JOIN FETCH r.time " +
+           "JOIN FETCH r.member " +
            "WHERE r.member.id = :memberId")
     List<Reservation> findByMemberId(Long memberId);
+
+    @Query("SELECT r FROM Reservation r " +
+           "JOIN FETCH r.theme " +
+           "JOIN FETCH r.time " +
+           "JOIN FETCH r.member " +
+           "WHERE r.id = :id")
+    Optional<Reservation> findById(Long id);
+
+    @Query("SELECT r FROM Reservation r " +
+           "JOIN FETCH r.theme " +
+           "JOIN FETCH r.time " +
+           "JOIN FETCH r.member")
+    List<Reservation> findAll();
 
     boolean existsByDateAndThemeAndTime(String date, Theme theme, Time time);
 }
